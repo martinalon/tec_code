f24b946913b57a31c1d47c1d0809cb3cc7e3c9a3
Tania Reyes Amador <taniareyesamador25@gmail.com>
2023-11-28 17:57:12 -0600

    Se agrega el logging y el __init__.py en la carpeta de api

--- api/ml_api.py
+++ api/ml_api.py
@@ -3,9 +3,9 @@ from pydantic import BaseModel
 from train.use_model import prediccion
 import logging
 
+
 logging.basicConfig(filename='bitacora.log', level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s')
-
 
 
 app = FastAPI()
@@ -25,21 +25,19 @@ class PassengerData(BaseModel):
     boat: str
     body: int
 
-# Endpoint to receive data via POST
+
 @app.post("/predict_survival")
 async def predict_survival(passenger_data: PassengerData):
     try:
         print(passenger_data.model_dump())
         my_model, name = prediccion(passenger_data.model_dump())
-        # Here you can perform any processing or prediction with the received data
-        # For now, let's just return the received data as a response
+        logging.info(f"Valores de entrada: {passenger_data.model_dump()}")
         result_dict = {
             "Received Data": int(my_model[0]),
             "Model Name":name }
+        logging.info(f"Prediccion: {result_dict}")
         return(result_dict)
     
     except Exception as e:
         logging.error(f" error in {e}")
     
-        
-
